11356f013e0237b3caedbd6193b5b429d50e3b7a
Amit <144780066+Amit931768@users.noreply.github.com>
2026-03-07 15:04:48 +0530

    some changes

--- app.py
+++ app.py
@@ -58,5 +58,8 @@ def analyze():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
