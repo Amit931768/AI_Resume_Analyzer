94de64459aee7671fd66885fbcc7582c278920ba
Amit <144780066+Amit931768@users.noreply.github.com>
2026-03-07 22:32:29 +0530

    some changes

--- static/script.js
+++ static/script.js
@@ -4,46 +4,57 @@ const dropArea = document.getElementById("dropArea");
 const fileInput = document.getElementById("fileInput");
 const dropText = document.getElementById("dropText");
 
-// CLICK UPLOAD (WORKS MOBILE + DESKTOP)
 
-dropArea.addEventListener("click", () => {
-fileInput.click();
+// CLICK UPLOAD (MOBILE + DESKTOP)
+
+dropArea.addEventListener("click", function(){
+    fileInput.click();
 });
 
-// FILE NAME SHOW
+
+// FILE NAME SHOW AFTER SELECT
 
 fileInput.addEventListener("change", function(){
 
-if(this.files.length > 0){
-dropText.innerText = this.files[0].name;
-}
+    if(fileInput.files.length > 0){
+        dropText.innerText = fileInput.files[0].name;
+    }
 
 });
 
-// DRAG OVER
 
-dropArea.addEventListener("dragover",(e)=>{
-e.preventDefault();
-dropArea.style.background="#eef2ff";
+// DRAG OVER (DESKTOP)
+
+dropArea.addEventListener("dragover", function(e){
+
+    e.preventDefault();
+    dropArea.style.background = "#eef2ff";
+
 });
+
 
 // DRAG LEAVE
 
-dropArea.addEventListener("dragleave",()=>{
-dropArea.style.background="#f8f9ff";
+dropArea.addEventListener("dragleave", function(){
+
+    dropArea.style.background = "#f8f9ff";
+
 });
 
-// DROP FILE
 
-dropArea.addEventListener("drop",(e)=>{
+// DROP FILE (DESKTOP)
 
-e.preventDefault();
+dropArea.addEventListener("drop", function(e){
 
-fileInput.files = e.dataTransfer.files;
+    e.preventDefault();
 
-dropText.innerText = e.dataTransfer.files[0].name;
+    const files = e.dataTransfer.files;
 
-dropArea.style.background="#f8f9ff";
+    if(files.length > 0){
+        dropText.innerText = files[0].name;
+    }
+
+    dropArea.style.background = "#f8f9ff";
 
 });
 
